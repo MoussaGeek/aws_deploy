1e040a06d8e5491800f4194e45f8080d14ef598b
MoussaGeek <moussadiakite1963@gmail.com>
2023-08-31 16:50:07 +0000

    deploy1

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock "~> 3.10"    # 1
 
 set :application, "blog_app"    # 2
-set :repo_url, "https://github.com/AbdoulKamiss/deploy_aws_task.git"    # 3
+set :repo_url, "https://github.com/MoussaGeek/aws_deploy"    # 3
 set :linked_files, %w{config/secrets.yml}   # 4
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}   # 5
 set :keep_releases, 5   # 6
